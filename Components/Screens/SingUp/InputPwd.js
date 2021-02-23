09d76dc3d88de65d9a0eb8ab3a0e27725510fa62
nightohl <haneojin@gmail.com>
2021-02-24 03:37:17 +0900

    chore-ESLint+Prettier 설정

--- Components/Screens/SingUp/InputPwd.js
+++ Components/Screens/SingUp/InputPwd.js
@@ -1,50 +1,62 @@
-
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
-import Feather from 'react-native-vector-icons/Feather'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
 
-export default function InputPwd({ data, textPasswordChange, updateChkSecure }) {
-    return (
-        <>
-            {/* 패스워드 입력란 */}
-            <Text style={styles.text_footer}>패스워드</Text>
-            <View style={styles.action}>
-                <Feather name="lock" color="#000" size={20} />
-                <TextInput style={styles.textInput}
-                    placeholder="Your Password"
-                    autoCapitalize="none"
-                    secureTextEntry={data.chk_secure} //텍스트 보이냐 숨기냐가 secureTextEntry state로 결정됨
-                    onChangeText={(pw) => textPasswordChange(pw)} //입력값이 password state에 반영됨.
-                />
+export default function InputPwd({
+  data,
+  textPasswordChange,
+  updateChkSecure,
+}) {
+  return (
+    <>
+      {/* 패스워드 입력란 */}
+      <Text style={styles.text_footer}>패스워드</Text>
+      <View style={styles.action}>
+        <Feather name="lock" color="#000" size={20} />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Password"
+          autoCapitalize="none"
+          secureTextEntry={data.chk_secure} //텍스트 보이냐 숨기냐가 secureTextEntry state로 결정됨
+          onChangeText={pw => textPasswordChange(pw)} //입력값이 password state에 반영됨.
+        />
 
-                {/* 눈 아이콘 클릭 마다 패스워드 보일지 숨길지 토글 */}
-                <TouchableOpacity onPress={updateChkSecure}>
-                    {data.chk_secure ?
-                        <Feather name="eye-off" color="gray" size={20} />
-                        : <Feather name="eye" color="gray" size={20} />}
-                </TouchableOpacity>
-            </View>
-        </>
-    );
+        {/* 눈 아이콘 클릭 마다 패스워드 보일지 숨길지 토글 */}
+        <TouchableOpacity onPress={updateChkSecure}>
+          {data.chk_secure ? (
+            <Feather name="eye-off" color="gray" size={20} />
+          ) : (
+            <Feather name="eye" color="gray" size={20} />
+          )}
+        </TouchableOpacity>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    text_footer: {
-        color: '#000',
-        fontSize: 18,
-    },
-    action: {
-        flexDirection: 'row',
-        marginTop: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#f2f2f2',
-        paddingBottom: 5,
-        marginBottom:20,
-    },
-    textInput: {
-        flex: 1,
-        marginTop: Platform.OS === 'ios' ? 0 : -12,
-        paddingLeft: 10,
-        color: '#000',
-    },
+  text_footer: {
+    color: '#000',
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5,
+    marginBottom: 20,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: '#000',
+  },
 });
--- Components/Screens/SingUp/SignUpComponent.js
+++ Components/Screens/SingUp/SignUpComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView, SafeAreaView } from 'react-native'
-import * as Animatable from 'react-native-animatable'
+import { StyleSheet, ScrollView, SafeAreaView } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 import Header from './Header';
 import InputEmail from './InputEmail';
@@ -12,52 +12,72 @@ import InputGender from './InputGender';
 import InputRole from './InputRole';
 import Buttons from './Buttons';
 
-export default function SignUpComponent({ navigation, data,
-    textEmailChange, textNameChange, textAgeChange, textGenderChange, textRoleChange,
-    textPasswordChange, textConrifmPasswordChange, updateChkSecure,
-    submitSingUP }) {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Header />
-            <Animatable.View style={styles.footer} animation="fadeInUpBig" >
-                <ScrollView>
-                    {/* 이메일 입력란 */}
-                    <InputEmail data={data} textEmailChange={textEmailChange} />
+export default function SignUpComponent({
+  navigation,
+  data,
+  textEmailChange,
+  textNameChange,
+  textAgeChange,
+  textGenderChange,
+  textRoleChange,
+  textPasswordChange,
+  textConrifmPasswordChange,
+  updateChkSecure,
+  submitSingUP,
+}) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <Animatable.View style={styles.footer} animation="fadeInUpBig">
+        <ScrollView>
+          {/* 이메일 입력란 */}
+          <InputEmail data={data} textEmailChange={textEmailChange} />
 
-                    {/* 패스워드 입력란 */}
-                    <InputPwd data={data} textPasswordChange={textPasswordChange} updateChkSecure={updateChkSecure} />
-                    {/* 패스워드 확인란 */}
-                    <InputPwdChk data={data} textConrifmPasswordChange={textConrifmPasswordChange} updateChkSecure={updateChkSecure} />
+          {/* 패스워드 입력란 */}
+          <InputPwd
+            data={data}
+            textPasswordChange={textPasswordChange}
+            updateChkSecure={updateChkSecure}
+          />
+          {/* 패스워드 확인란 */}
+          <InputPwdChk
+            data={data}
+            textConrifmPasswordChange={textConrifmPasswordChange}
+            updateChkSecure={updateChkSecure}
+          />
 
-                    {/* 이름 입력란 */}
-                    <InputName data={data} textNameChange={textNameChange} />
-                    {/* 나이 입력란 */}
-                    <InputAge data={data} textAgeChange={textAgeChange} />
-                    {/* 성별 입력란 */}
-                    <InputGender data={data} textGenderChange={textGenderChange} />
-                    {/* 유저 유형 선택란 */}
-                    <InputRole data={data} textRoleChange={textRoleChange} />
+          {/* 이름 입력란 */}
+          <InputName data={data} textNameChange={textNameChange} />
+          {/* 나이 입력란 */}
+          <InputAge data={data} textAgeChange={textAgeChange} />
+          {/* 성별 입력란 */}
+          <InputGender data={data} textGenderChange={textGenderChange} />
+          {/* 유저 유형 선택란 */}
+          <InputRole data={data} textRoleChange={textRoleChange} />
 
-                    {/* 버튼 */}
-                    <Buttons data={data} navigation={navigation} submitSingUP={submitSingUP} />
-                </ScrollView>
-            </Animatable.View>
-
-        </SafeAreaView>
-    )
+          {/* 버튼 */}
+          <Buttons
+            data={data}
+            navigation={navigation}
+            submitSingUP={submitSingUP}
+          />
+        </ScrollView>
+      </Animatable.View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#000'
-    },
-    footer: {
-        flex: 7,
-        backgroundColor: '#fff',
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        paddingHorizontal: 20,
-        paddingVertical: 30
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  footer: {
+    flex: 7,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
 });
